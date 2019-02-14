beddf80829b4faea08e7076948a51ffa70b10485
Frank-Huang-bo <44944715+Frank-Huang-bo@users.noreply.github.com>
2019-02-14 21:32:16 +0800

    VUE電商

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+     <a href="#" @click.prevent="signout">登出</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    };
+  },
+  methods: {
+	    signout() {
+	      const api = `${process.env.APIPATH}/logout`;
+	      const vm = this;
+	      this.$http.post(api).then((response) => {
+	        console.log(response.data);
+	        if (response.data.success) {
+	          vm.$router.push('/login');
+	        }
+	      });
+	    },
+	  },
+};
+</script>
+
